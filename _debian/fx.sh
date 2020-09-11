41f38f8021ed0829460ea62163180cfe15fcbddf
Luis Betancourt <webdeveloperpr@gmail.com>
2020-09-10 21:50:03 -0400

    Create ALB stack and update the cf script

---
+++ _debian/fx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+######################################################################
+# fx
+######################################################################
+
+wget https://github.com/antonmedv/fx/releases/download/14.0.1/fx-linux.zip
+unzip fx-linux.zip
+sudo mv fx-linux /usr/local/bin/fx
